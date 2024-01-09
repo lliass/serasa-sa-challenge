6757aa702626ace809631f04892ed5d46589a3fe
Elias Lopes <elias.lopes@mevosaude.com.br>
2024-01-09 16:31:26 +1100

    feat(Project): Project Structure Finished

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-// First Commit
+import Application from './application';
+import { PostgresDataSource } from './infrastructure/persistence/postgres/data-source';
+
+const application = new Application();
+
+PostgresDataSource.initialize()
+  .then(async () => {
+    console.log({
+      message: 'Postgres for api has been initialized',
+    });
+
+    application.start();
+  })
+  .catch((error) =>
+    console.error({
+      errorMessage: 'An error on initialization of api application',
+      errorStack: error,
+    }),
+  );
